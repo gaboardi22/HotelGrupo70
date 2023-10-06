966e7a902f16c7dfde72a1ea05789bc64f506d25
gaboardi22 <andresgaboardi@gmail.com>
2023-10-05 21:29:22 -0300

    .

--- src/AccesoDatos/HabitacionData.java
+++ src/AccesoDatos/HabitacionData.java
@@ -2,29 +2,29 @@
 package AccesoDatos;
 
 import Entidades.Habitacion;
+import Entidades.TipoHabitacion;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 
 public class HabitacionData {
     private Connection con;
+    private TipoHabitacion tHab;
 
     public HabitacionData(Connection con) {
         con = Conexion.getConexion();
     }
     
     public void crearHabitacion(Habitacion habitacion){
-   
-        String SQL = "INSERT INTO habitacion (estado, tipoHabitacion,piso) VALUES (?,?,?)";
+           
+        String SQL = "INSERT INTO habitacion (estado, tipoHabitacion, piso) VALUES (?,?,?)";
         try {
             
             PreparedStatement ps = con.prepareStatement(SQL);
             ps.setBoolean(1, true);
-            ps.se
+            
         } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "Error al conectar a la BDD");
         }
