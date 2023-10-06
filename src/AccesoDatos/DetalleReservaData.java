b31befa1db22b9b01888dd07c2405c4a5de1d78a
wtexnet <waltergabriel@gmail.com>
2023-10-06 20:57:23 -0300

    Create DetalleReservaData.java

---
+++ src/AccesoDatos/DetalleReservaData.java
@@ -0,0 +1,17 @@
+
+package AccesoDatos;
+
+import java.sql.Connection;
+
+
+public class DetalleReservaData {
+    private Connection con;
+
+    public DetalleReservaData(Connection con) {
+        con = Conexion.getConexion();
+    }
+    
+public void nuevoDetalleReservaData(Reserva reserva, Habitacion habitacion)
+        DetalleReserva detallere
+    
+}
